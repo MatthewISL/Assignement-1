a0017f93d9d31ffb42cc1226765f7dc502e462f1
MatthewISL <mate.siget@gmail.com>
2022-10-06 09:47:17 +0200

    Add project files.

---
+++ FootballPlayer/Class1.cs
@@ -0,0 +1,55 @@
+﻿namespace FootballPlayer
+{
+    //dd
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+
+
+        public void ValidateId()
+        {
+
+            if (Id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Id));
+            }
+        }
+        public void ValidateName()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentNullException(nameof(Name));
+            }
+            if (Name.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Name));
+            }
+        }
+        public void ValidateAge()
+        {
+            if (Age < 18)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Age));
+            }
+        }
+        public void ValidateShirtNumber()
+        {
+            if (ShirtNumber < 1 || ShirtNumber > 99)
+            {
+                throw new ArgumentOutOfRangeException(nameof(ShirtNumber));
+            }
+        }
+
+        public void Validate()
+        {
+            ValidateId();
+            ValidateName();
+            ValidateAge();
+            ValidateShirtNumber();
+        }
+    }
+}
---
+++ FootballPlayerTests/FootballPlayerTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FootballPlayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FootballPlayer.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        FootballPlayer badPlayer = new FootballPlayer() { Id = -1, Name = "B", Age = 13, ShirtNumber = 765 };
+        FootballPlayer goodPlayer = new FootballPlayer() { Id = 3, Name = "George", Age = 24, ShirtNumber = 47 };
+
+        [TestMethod()]
+        public void ValidateIdTest()
+        {
+            goodPlayer.ValidateId();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => badPlayer.ValidateId());
+
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(() => goodPlayer.ValidateId());
+        }
+        [TestMethod()]
+        public void ValidateName()
+        {
+            goodPlayer.ValidateName();
+            FootballPlayer player1 = new FootballPlayer() { Id = 7, Name = null, Age = 32, ShirtNumber = 39 };
+            Assert.ThrowsException<ArgumentNullException>(() => player1.ValidateName());
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => badPlayer.ValidateName());
+
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(() => goodPlayer.ValidateName());
+
+        }
+        [TestMethod()]
+        public void ValidateAge()
+        {
+            goodPlayer.ValidateAge();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => badPlayer.ValidateAge());
+
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(() => goodPlayer.ValidateAge());
+        }
+        [TestMethod()]
+        public void ValidateShirtNumber()
+        {
+            goodPlayer.ValidateShirtNumber();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => badPlayer.ValidateShirtNumber());
+
+            FootballPlayer player2 = new FootballPlayer() { Id = 4, Name = "Bob", Age = 43, ShirtNumber = -3 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => player2.ValidateShirtNumber());
+
+            //Assert.ThrowsException<ArgumentOutOfRangeException>(() => goodPlayer.ValidateAge());
+
+        }
+    }
+}
